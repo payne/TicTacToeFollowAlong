ebad296ffdade19674d057b332087003153a8917
Matt Payne <Payne@MattPayne.org>
2019-09-21 16:56:04 -0500

    Made squares look good.

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,8 +2,12 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  template: `<button>{{ value }}<button>`,
-  styles: []
+  template: `
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+  `,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent {
 
